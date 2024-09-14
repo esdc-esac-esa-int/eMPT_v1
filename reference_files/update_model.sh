eb72f88db5444d61f501855444da03366d1fc361
ninarb <44358584+nbonaventura@users.noreply.github.com>
2024-09-13 23:13:19 -0700

    Add files via upload
    Signed-off-by: ninarb <44358584+nbonaventura@users.noreply.github.com>

--- reference_files/update_model.sh
+++ reference_files/update_model.sh
@@ -12,7 +12,7 @@ python read_siaf_file.py
 # ./fits_siaf_read_v2v3
 
 gfortran -ffree-line-length-none -fno-backslash -fno-range-check -O2 -o calc_derived_parameters calc_derived_parameters.f90 \
--L/usr/local/lib/pgplot/ -lpgplot -L/usr/lib -lgcc  -L/usr/X11/lib -lX11
+-L/usr/local/lib/pgplot/ -lpgplot -L/usr/lib -lgcc  -lcfitsio -L/usr/X11/lib -lX11
 
 ./calc_derived_parameters
 
